582f531ef714df65fdc4e543b05403584fb2fdaa
Aaron Meese <ajmeese7@gmail.com>
2024-06-30 23:49:34 +0000

    feat: initial Fly config

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
-const puppeteer = require('puppeteer');
-const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000 // Heroku or local
-var app = express();
+const puppeteer = require("puppeteer");
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const app = express();
 
 app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .get('/index', (req, res) => res.render('index'))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("index"))
+  .get("/index", (req, res) => res.render("index"))
   .use((req, res, next) => {
       // CORS headers
-      res.append('Access-Control-Allow-Origin', ['*']);
-      res.append('Access-Control-Allow-Methods', 'GET');
-      res.append('Access-Control-Allow-Headers', 'Content-Type');
+      res.append("Access-Control-Allow-Origin", ["*"]);
+      res.append("Access-Control-Allow-Methods", "GET");
+      res.append("Access-Control-Allow-Headers", "Content-Type");
       next();
   })
-  .get('/generate', function (req, res) {
-      let text_param = req.query.text;
-      let textColor = req.query.textColor;
-      let backgroundColor = req.query.backgroundColor;
-      let shadow = req.query.shadow;
-      let ascii_url = "http://patorjk.com/software/taag/#p=display&f=Alpha&t=" + encodeURIComponent(text_param);
-      if (!text_param) return res.render('error');
+  .get("/generate", function (req, res) {
+      const { text_param, textColor, backgroundColor, shadow } = req.query;
+      if (!text_param) return res.render("error");
 
       (async () => {
-        const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
+        const browser = await puppeteer.launch({args: ["--no-sandbox", "--disable-setuid-sandbox"]})
         const page = await browser.newPage()
 
+        const ascii_url = "http://patorjk.com/software/taag/#p=display&f=Alpha&t=" + encodeURIComponent(text_param);
         await page.goto(ascii_url)
-          .catch(err => {
-            console.error(err);
-
-            var obj = { err : err };
-            res.writeHead(404, {"Content-Type": "application/json"}); // Error code
-            res.write(JSON.stringify(obj));
+          .catch((err) => {
+            console.error("Error getting ascii_url page:", err);
+            res.writeHead(404, {"Content-Type": "application/json"});
+            res.write(JSON.stringify({ err }));
             res.end();
           });
-        
+
         // Get the ASCII from the page
-        const ascii = await page.$eval('#taag_output_text', el => el.textContent);
+        const ascii = await page.$eval("#taag_output_text", (el) => el.textContent);
 
         // Navigate to the ASCII tools site
         await page.goto("https://onlineasciitools.com/convert-ascii-to-image")
         await page.select('[data-index="typeface"]', "monospace")
         await page.$eval('[data-index="background-color"]', (el, color) => el.value = color, backgroundColor);
         await page.$eval('[data-index="text-color"]', (el, color) => el.value = color, textColor);
-        await page.$eval('[data-index="font-size"]', el => el.value = "12px");
+        await page.$eval('[data-index="font-size"]', (el) => el.value = "12px");
 
         if (shadow == "true")
           await page.$eval('[data-index="text-shadow"]', (el, color) => el.value = `-1ex 0.2pc 6px ${color}`, textColor);
@@ -55,26 +50,27 @@ app
         // https://github.com/puppeteer/puppeteer/issues/3347#issuecomment-427234299
         const boldCheckbox = '[data-index="bold"]';
         await page.evaluate((boldCheckbox) => document.querySelector(boldCheckbox).click(), boldCheckbox);
-        
+
         // Places ASCII from other website into the textarea
-        await page.$eval('.data-wrapper textarea', (el, input) => el.value = input, ascii);
-        await page.type('.data-wrapper textarea', " "); // To make sure input is recognized
+        await page.$eval(".data-wrapper textarea", (el, input) => el.value = input, ascii);
+        // To make sure input is recognized
+        await page.type(".data-wrapper textarea", " ");
 
         // Generate base64 image URL
         const dataUri = '[data-url="convert-image-to-data-uri"]';
         await page.click('.output [data-toggle="toggle-chain"]');
         await page.waitForSelector(dataUri);
-        page.$eval(dataUri, elem => elem.click());
+        page.$eval(dataUri, (elem) => elem.click());
 
         // Unique selector for the right copy button. Don't ask questions, just leave it alone. Please.
         await page.waitForSelector("div.tool-chained>div:nth-child(4)>div:nth-child(1)>div:nth-child(2)>div>div:nth-child(2)>div:nth-child(1)>div:nth-child(4)");
-        const base64_url = await page.evaluate(_ => {
+        const base64_url = await page.evaluate((_) => {
           // Press the copy button, which selects the text
-          let copy = document.getElementsByClassName("widget-copy")[3];
+          const copy = document.getElementsByClassName("widget-copy")[3];
           copy.click();
 
           // Get and return the selected text (the base64 URL)
-          let selection = window.getSelection().toString();
+          const selection = window.getSelection().toString();
           return selection;
         });
 
@@ -85,5 +81,5 @@ app
         await browser.close()
       })()
   })
-  .get('*', (req, res) => res.render('error'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`)) // localhost:5000
+  .get("*", (req, res) => res.render("error"))
+  .listen(PORT, () => console.info(`Listening on ${PORT}`));
--- public/index.js
+++ public/index.js
@@ -1,79 +1,78 @@
 // Waits until the document's elements are available
-document.addEventListener("DOMContentLoaded", function(event) {
-    let generateButton = document.getElementById("generateButton");
+document.addEventListener("DOMContentLoaded", function (event) {
+  const generateButton = document.getElementById("generateButton");
+  generateButton.disabled = true;
+  const downloadButton = document.getElementById("downloadButton");
+  downloadButton.disabled = true;
+
+  // Disables button if asciiText empty; otherwise enabled
+  const asciiText = document.getElementById("asciiText");
+  asciiText.oninput = () => {
+    if (!!asciiText.value) return generateButton.disabled = false;
     generateButton.disabled = true;
-    let downloadButton = document.getElementById("downloadButton");
-    downloadButton.disabled = true;
+  };
 
-    // Disables button if asciiText empty; otherwise enabled
-    let asciiText = document.getElementById("asciiText");
-    asciiText.oninput = () => {
-        if (!!asciiText.value) return generateButton.disabled = false;
-        generateButton.disabled = true;
-    };
-
-    // Configures advanced settings dropdown
-    let advanced = document.getElementById("advanced");
-    let dropdown = document.getElementById("dropdown");
-    dropdown.onclick = () => {
-        // https://stackoverflow.com/a/21696585/6456163
-        if (advanced.offsetParent === null) {
-            // TODO: Add a class that animates this transition
-            advanced.style.display = "block";
-            dropdown.innerText = "▲";
-        } else {
-            advanced.style.display = "none";
-            dropdown.innerText = "▼";
-        }
+  // Configures advanced settings dropdown
+  const advanced = document.getElementById("advanced");
+  const dropdown = document.getElementById("dropdown");
+  dropdown.onclick = () => {
+    // https://stackoverflow.com/a/21696585/6456163
+    if (advanced.offsetParent === null) {
+      // TODO: Add a class that animates this transition
+      advanced.style.display = "block";
+      dropdown.innerText = "▲";
+    } else {
+      advanced.style.display = "none";
+      dropdown.innerText = "▼";
     }
+  }
 });
 
 function generateImage() {
-    // Unhide loader, hide previous image, and disable download
-    let spinner = document.getElementsByClassName("spinner-border")[0];
-    spinner.style.display = 'block';
-    let image = document.getElementById("result");
-    image.style.visibility = "hidden";
-    let downloadButton = document.getElementById("downloadButton");
-    downloadButton.disabled = true;
+  // Unhide loader, hide previous image, and disable download
+  const spinner = document.getElementsByClassName("spinner-border")[0];
+  spinner.style.display = "block";
+  const image = document.getElementById("result");
+  image.style.visibility = "hidden";
+  const downloadButton = document.getElementById("downloadButton");
+  downloadButton.disabled = true;
 
-    let asciiText = document.getElementById("asciiText").value;
-    let textColor = document.getElementById("txt-color").value;
-    let backgroundColor = document.getElementById("bg-color").value;
-    let shadow = document.getElementById("shadow").checked;
-    let url = `/generate?text=${asciiText}&textColor=${textColor}&backgroundColor=${backgroundColor}&shadow=${shadow}`;
-    url = encodeURI(url);
+  const asciiText = document.getElementById("asciiText").value;
+  const textColor = document.getElementById("txt-color").value;
+  const backgroundColor = document.getElementById("bg-color").value;
+  const shadow = document.getElementById("shadow").checked;
+  const url = encodeURI(`/generate?text=${asciiText}&textColor=${textColor}&backgroundColor=${backgroundColor}&shadow=${shadow}`);
 
-    let client = new HttpClient();
-    client.get(url, function(response) {
-        // Set download href to the image, and the name to the text,
-        // but with spaces replaced with underscores
-        let download = document.getElementById("download");
-        download.href = response;
-        download.download = `${asciiText.split(' ').join('_')}.png`;
-        
-        // Allow the download button
-        downloadButton.disabled = false;
+  const client = new HttpClient();
+  client.get(url, function (response) {
+    // Set download href to the image, and the name to the text,
+    // but with spaces replaced with underscores
+    const download = document.getElementById("download");
+    download.href = response;
+    download.download = `${asciiText.split(" ").join("_")}.png`;
 
-        // Set image src to retrieved image and display it
-        image.setAttribute('src', response);
-        image.style.visibility = "visible";
+    // Allow the download button
+    downloadButton.disabled = false;
 
-        // Hide loader
-        spinner.style.display = 'none';
-    });
+    // Set image src to retrieved image and display it
+    image.setAttribute("src", response);
+    image.style.visibility = "visible";
+
+    // Hide loader
+    spinner.style.display = "none";
+  });
 }
 
 // https://stackoverflow.com/a/22076667/6456163
-var HttpClient = function() {
-    this.get = function(aUrl, aCallback) {
-        var anHttpRequest = new XMLHttpRequest();
-        anHttpRequest.onreadystatechange = function() { 
-            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.responseText);
-        }
-
-        anHttpRequest.open( "GET", aUrl, true );            
-        anHttpRequest.send( null );
+var HttpClient = function () {
+  this.get = function (aUrl, aCallback) {
+    const anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function () {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+        aCallback(anHttpRequest.responseText);
     }
+
+    anHttpRequest.open("GET", aUrl, true);
+    anHttpRequest.send(null);
+  }
 }
